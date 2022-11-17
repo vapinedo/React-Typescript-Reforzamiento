602c022d2bf9508bd6d25d6f8c5da06ae61b525e
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-11-16 21:21:30 -0500

    11. Tipos Básicos - TypeScript

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,7 +1,16 @@
 export const TiposBasicos = () => {
+
+    const nombre: string = 'Victor';
+    const edad: number = 22;
+    const estaActivo = true;
+    const listaPoderes = ['Volar', 'Velocidad'];
+
   return (
     <>
         <h3>Tipos Básicos</h3>
+        {nombre}, {edad}, {estaActivo.toString()}
+        <br />
+        {listaPoderes.join(', ')}
     </>
   )
 }
